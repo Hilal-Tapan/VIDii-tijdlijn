c54f674e52cc90228ff5e9aa250e84362a85c4fd
“Hilal-Tapan“ <hilalt98@hotmail.com>
2022-11-03 11:41:49 +0100

    animaties

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
 console.log("Howdy!");
+
+
+
+// If you need to hide an element a few seconds after a button is clicked: 
+// Add an event listener to the button element. Inside of the event handler function, 
+// use the setTimeout method to invoke a function after a delay. Set the element's display property to none 
+
+var buttonn = document.querySelector("button");
+buttonn.addEventListener("click", coinGone);
+
+function coinGone () {
+    var firstCoin = document.querySelector("main section:first-of-type img:nth-of-type(4)");
+    firstCoin.classList.add("gone");
+}
+
+
+//   function doHide(){
+//     document.getElementById( "imgHideShow" ).style.display = "none" ;
+// }
+//   function hideImage(){
+//     setTimeout( "doHide()", 4000 ) ;
+// }
