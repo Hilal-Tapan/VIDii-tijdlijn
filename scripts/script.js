a2d6474ce44a0b0fd790a5daad5e0d195c94831b
“Hilal-Tapan“ <hilalt98@hotmail.com>
2022-11-07 14:28:30 +0100

    javascriptsss

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,45 @@
 // JavaScript Document
 console.log("Howdy!");
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Vind princess 1
+var findPrincess1 = document.querySelector("section:nth-of-type(2) ul li:nth-of-type(1)");
+var princes1 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(2)");
 
+findPrincess1.addEventListener("click", ()=> {
+    princes1.classList.add('zoekPrincess1')
+})
+
+// tekst wolkje princess 1
+var wolkje1 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(2) button");
+
+wolkje1.addEventListener("click", () =>{
+    princes1.classList.toggle('tekstWolkje1')
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Vind princess 2
+var findPrincess2 = document.querySelector("section:nth-of-type(2) ul li:nth-of-type(2)");
+var princes2 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(3)");
+
+findPrincess2.addEventListener("click", ()=> {
+    princes2.classList.add('zoekPrincess2')
+})
+
+// tekst wolkje princess 2
+var wolkje2 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(3) button");
+
+wolkje2.addEventListener("click", () =>{
+    princes2.classList.toggle('tekstWolkje2')
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Vind princes 3
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 // Animaties starten na klikken op de knop (sterretjes, princes en startknop)
 var startButton = document.querySelector("header button");
 
