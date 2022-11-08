b49f7f7b8bf4cec1f5a93e7fce7e13140c0064bf
“Hilal-Tapan“ <hilalt98@hotmail.com>
2022-11-08 11:48:41 +0100

    paaseieren

--- scripts/script.js
+++ scripts/script.js
@@ -6,31 +6,30 @@ console.log("Howdy!");
 var findPrincess1 = document.querySelector("section:nth-of-type(2) ul li:nth-of-type(1)");
 var princes1 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(2)");
 
-findPrincess1.addEventListener("click", ()=> {
+findPrincess1.addEventListener("click", () => {
     princes1.classList.add('zoekPrincess1')
 })
 
 // tekst wolkje princess 1
 var wolkje1 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(2) button");
 
-wolkje1.addEventListener("click", () =>{
+wolkje1.addEventListener("click", () => {
     princes1.classList.toggle('tekstWolkje1')
 })
 
 /////////////////////////////////////////////////////////////////////////////////////////////
-
 // Vind princess 2
 var findPrincess2 = document.querySelector("section:nth-of-type(2) ul li:nth-of-type(2)");
 var princes2 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(3)");
 
-findPrincess2.addEventListener("click", ()=> {
+findPrincess2.addEventListener("click", () => {
     princes2.classList.add('zoekPrincess2')
 })
 
 // tekst wolkje princess 2
 var wolkje2 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(3) button");
 
-wolkje2.addEventListener("click", () =>{
+wolkje2.addEventListener("click", () => {
     princes2.classList.toggle('tekstWolkje2')
 })
 
@@ -39,14 +38,14 @@ wolkje2.addEventListener("click", () =>{
 var findPrincess3 = document.querySelector("section:nth-of-type(2) ul li:nth-of-type(3)");
 var princes3 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(5)");
 
-findPrincess3.addEventListener("click", ()=> {
+findPrincess3.addEventListener("click", () => {
     princes3.classList.add('zoekPrincess3')
 })
 
 // tekst wolkje princess 3
 var wolkje3 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(5) button");
 
-wolkje3.addEventListener("click", () =>{
+wolkje3.addEventListener("click", () => {
     princes3.classList.toggle('tekstWolkje3')
 })
 
@@ -55,7 +54,7 @@ wolkje3.addEventListener("click", () =>{
 var princes4 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(1)");
 var wolkje4 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(1) button");
 
-wolkje4.addEventListener("click", () =>{
+wolkje4.addEventListener("click", () => {
     princes4.classList.toggle('tekstWolkje4')
 })
 
@@ -63,7 +62,7 @@ wolkje4.addEventListener("click", () =>{
 var princes5 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(4)");
 var wolkje5 = document.querySelector("section:nth-of-type(2) > ol li:nth-of-type(4) button");
 
-wolkje5.addEventListener("click", () =>{
+wolkje5.addEventListener("click", () => {
     princes5.classList.toggle('tekstWolkje5')
 })
 
@@ -76,12 +75,13 @@ startButton.addEventListener("click", start);
 function start() {
     muntjesWeg();
 
- startButton.classList.add("buttonStart")
+    startButton.classList.add("buttonStart")
 
-var peach1 = document.querySelector("#peach1");
-peach1.classList.add("springen");
+    var peach1 = document.querySelector("#peach1");
+    peach1.classList.add("springen");
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////
 // variabalen muntjes
 var muntSelector = 'section:nth-of-type(1) > img:nth-of-type('
 var muntje1 = document.querySelector(muntSelector + '4)')
@@ -92,55 +92,55 @@ var muntje5 = document.querySelector(muntSelector + '8)')
 var muntje6 = document.querySelector(muntSelector + '9)')
 var muntje7 = document.querySelector(muntSelector + '10)')
 var muntje8 = document.querySelector(muntSelector + '11)')
-var muntje9 = document.querySelector(muntSelector + '12)')  
+var muntje9 = document.querySelector(muntSelector + '12)')
 
 
 // Ik heb met muntje 1 hulp gekregen van Jeffrey maar 
 // ik begrijp de code en heb de rest van de muntjes zelf uitgewerkt.
 
-function muntjesWeg(){
+function muntjesWeg() {
 
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje1.classList.add('gone')
     }, 250);
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje2.classList.add('gone')
     }, 400);
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje3.classList.add('gone')
     }, 500);
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje4.classList.add('gone')
     }, 600);
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje5.classList.add('gone')
     }, 700);
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje6.classList.add('gone')
     }, 1300);
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje7.classList.add('gone')
     }, 1500);
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje8.classList.add('gone')
     }, 1550);
-    setTimeout(()=>{
+    setTimeout(() => {
         muntje9.classList.add('gone')
     }, 1650);
 }
 
 
-    // variabelen timer
-    // var muntjes = 0;
-    // var mijnInterval = setInterval( function(){
-    //     muntjes++;
-    //     console.log(muntjes)
-    //     if(muntjes > 8){
-    //         clearInterval(mijnInterval);
-    //         console.log('klaar')
-    //     }
-    // }, 1000);
-    // Waarom interval als ze aangaf dat de muntjes op verschillende tijden moesten verdwijnen?
+// variabelen timer
+// var muntjes = 0;
+// var mijnInterval = setInterval( function(){
+//     muntjes++;
+//     console.log(muntjes)
+//     if(muntjes > 8){
+//         clearInterval(mijnInterval);
+//         console.log('klaar')
+//     }
+// }, 1000);
+// Waarom interval als ze aangaf dat de muntjes op verschillende tijden moesten verdwijnen?
 
 //   function doHide(){
 //     document.getElementById( "imgHideShow" ).style.display = "none" ;
